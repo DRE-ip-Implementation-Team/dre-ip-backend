8bda8ebcf03778cf0ea3f41f5b9993165dfc3986
wabisabia <bkkchris.dunn@outlook.com>
2021-11-10 01:38:26 +0000

    Add authentication endpoints

--- src/api.rs
+++ src/api.rs
@@ -1,15 +1,15 @@
 use rocket::Route;
 
+mod election_management;
 mod information;
 mod login;
-mod election_management;
 mod voting;
 
 pub fn routes() -> Vec<Route> {
     let mut routes = Vec::new();
-    routes.append(&mut information::routes());
-    routes.append(&mut login::routes());
-    routes.append(&mut election_management::routes());
-    routes.append(&mut voting::routes());
-    return routes;
+    routes.extend(information::routes());
+    routes.extend(login::routes());
+    routes.extend(election_management::routes());
+    routes.extend(voting::routes());
+    routes
 }
