7cb6c645f19b0f5cbb41ca0f4e272722be6f4e3b
wabisabia <bkkchris.dunn@outlook.com>
2021-12-11 16:04:30 +0000

    Fetch `admins` collection correctly in `setup.js`

--- setup.js
+++ setup.js
@@ -5,5 +5,5 @@ db.createCollection("voters");
 voters = db.getCollection("voters");
 voters.createIndex({sms: 1}, {unique: true});
 db.createCollection("admins");
-admins = db.getCollection("users");
+admins = db.getCollection("admins");
 admins.createIndex({username: 1}, {unique: true});
