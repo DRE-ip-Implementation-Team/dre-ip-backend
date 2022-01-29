cacafa92731fba592bcba0b6eba553d40f3cba0f
wabisabia <bkkchris.dunn@outlook.com>
2022-01-29 21:23:48 +0000

    Refactor `db_test` and correct its documentation

--- db_test/src/lib.rs
+++ db_test/src/lib.rs
@@ -10,18 +10,22 @@ use syn::{parse_macro_input, spanned::Spanned, FnArg, ItemFn, Pat, Signature, Ty
 /// If a panic occurs via a failed assertion or other unwinding panic, the [`mongodb::Database`] is
 /// cleared, and the panic is "rethrown".
 ///
-/// Note: this attribute requires that [`rocket::async_test`], `client_with_db` and `clear_db` are
-/// in scope. Additionally, the `futures` crate must be included as a test dependency so we can use
-/// [`FutureExt::catch_unwind`].
+/// Note: this attribute requires that [`rocket::async_test`] and `client_and_db` are
+/// in scope. A dev dependency on `tokio` is required to run each test future within the closure
+/// passed to [`std::panic::catch_unwind`].
 pub fn db_test(_: TokenStream, input: TokenStream) -> TokenStream {
     let mut item_fn = parse_macro_input!(input as ItemFn);
-    let sig = item_fn.sig.clone();
-    let name = sig.ident.clone();
-    if let Err(err) = check_sig(sig.clone()) {
+
+    // Reject invalid function signatures
+    if let Err(err) = check_sig(item_fn.sig.clone()) {
         return err.into_compile_error().into();
     }
-    let new_name = format_ident!("{}_test", name);
+
+    // Rename the future so the test is named as expected
+    let name = item_fn.sig.ident;
+    let new_name = format_ident!("{}_fut", name);
     item_fn.sig.ident = new_name.clone();
+
     quote! {
         #[rocket::async_test]
         async fn #name() {
@@ -29,24 +33,26 @@ pub fn db_test(_: TokenStream, input: TokenStream) -> TokenStream {
 
             #item_fn
 
-            // To avoid futures not being transferable across Unwind boundaries:
+            // `Mutex<T>: UnwindSafe` circumvents `T: !UnwindSafe`:
             // - See https://stackoverflow.com/a/66529014/13112498
             let client_mutex = std::sync::Mutex::new(client);
             let db_mutex = std::sync::Mutex::new(db.clone());
 
             let result = std::panic::catch_unwind(|| {
+                // Transfer mutexes across the unwind boundary
+                // Unwraps are valid as no poisoning occurs: mutexes are not given no other threads
                 let client = client_mutex.into_inner().unwrap();
                 let db = db_mutex.into_inner().unwrap();
 
+                // Manually run future inside
                 let handle = tokio::runtime::Handle::current();
-
                 let _ = handle.enter();
-
                 futures::executor::block_on(#new_name(client, db));
             });
 
             db.drop(None).await.unwrap();
 
+            // Panic with the original error
             if let Err(cause) = result {
                 std::panic::panic_any(cause);
             }
@@ -55,12 +61,17 @@ pub fn db_test(_: TokenStream, input: TokenStream) -> TokenStream {
     .into()
 }
 
+/// Ensure signature conforms to `async fn test_ident(client_ident: Client, db_ident: Database)`.
 fn check_sig(sig: Signature) -> Result<(), syn::Error> {
+    if sig.asyncness.is_none() {
+        return Err(syn::Error::new(sig.span(), "Test must be marked `async`"));
+    }
+
     let inputs = sig.inputs;
     if inputs.len() != 2 {
         return Err(syn::Error::new(
             inputs.span(),
-            "Arguments must be a `rocket::local::asynchronous::Client` and a `mongodb::Database`",
+            "Test arguments must be a `rocket::local::asynchronous::Client` and a `mongodb::Database`",
         ));
     }
 
@@ -69,49 +80,30 @@ fn check_sig(sig: Signature) -> Result<(), syn::Error> {
 
     for input in &inputs {
         if let FnArg::Typed(pat_type) = input {
-            if let Pat::Ident(_) = &*pat_type.pat {
+            if let Pat::Ident(_) = *pat_type.pat {
                 if let Type::Path(type_path) = &*pat_type.ty {
-                    let path = &type_path.path;
-                    let ty_ident = path
-                        .get_ident()
-                        .ok_or_else(|| {
-                            syn::Error::new(
-                                path.span(),
-                                "Type must be a standalone type identifier",
-                            )
-                        })?
-                        .to_string();
-                    match ty_ident.as_str() {
-                        "Client" => has_client = true,
-                        "Database" => has_db = true,
-                        _ => {
-                            return Err(syn::Error::new(
-                                ty_ident.span(),
-                                "Expected either `Client` or `Database`",
-                            ))
+                    if let Some(type_ident) = type_path.path.get_ident() {
+                        let raw_type_ident = type_ident.to_string();
+                        match raw_type_ident.as_str() {
+                            "Client" => {
+                                has_client = true;
+                                continue;
+                            }
+                            "Database" => {
+                                has_db = true;
+                                continue;
+                            }
+                            _ => {}
                         }
                     }
-                } else {
-                    return Err(syn::Error::new(
-                        pat_type.ty.span(),
-                        format!(
-                            "Function argument type must be a type identifier, got {:?} instead",
-                            pat_type.ty
-                        ),
-                    ));
                 }
-            } else {
-                return Err(syn::Error::new(
-                    pat_type.pat.span(),
-                    "Function argument pattern must be an identifier",
-                ));
             }
-        } else {
-            return Err(syn::Error::new(
-                input.span(),
-                format!("Function argument must not be a receiver type"),
-            ));
         }
+
+        return Err(syn::Error::new(
+            input.span(),
+            "Expected one of `client_ident: Client` or `db_ident: Database`",
+        ));
     }
 
     if has_client && has_db {
@@ -119,7 +111,7 @@ fn check_sig(sig: Signature) -> Result<(), syn::Error> {
     } else {
         Err(syn::Error::new(
             inputs.span(),
-            "The tagged function must accept a `rocket::local::asynchronous::Client` and `mongodb::Database`",
+            "Test must accept a `rocket::local::asynchronous::Client` and `mongodb::Database`",
         ))
     }
 }
