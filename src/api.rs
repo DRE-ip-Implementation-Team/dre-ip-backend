b737d50f4592d48accf2d05d90ae397bfddfb467
wabisabia <bkkchris.dunn@outlook.com>
2022-01-29 18:18:52 +0000

    Fix unrenamed `voter` module, now `group`

--- src/api.rs
+++ src/api.rs
@@ -2,14 +2,14 @@ use rocket::Route;
 
 mod admin;
 mod auth;
+mod group;
 mod public;
-mod voter;
 
 pub fn routes() -> Vec<Route> {
     let mut routes = Vec::new();
     routes.extend(admin::routes());
     routes.extend(public::routes());
     routes.extend(auth::routes());
-    routes.extend(voter::routes());
+    routes.extend(group::routes());
     routes
 }
