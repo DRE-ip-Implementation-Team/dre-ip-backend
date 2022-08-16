9cc01a459e48f25c15f5b26f899e79d5a3cefc43
Matthew Bardsley <Matthew.Bardsley@warwick.ac.uk>
2022-08-16 22:42:09 +0100

    dev: further changes to Dockerfiles

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed -i "s|AWS_ACCESS_KEY_ID|${AWS_ACCESS_KEY_ID}|" /home/dreip/.aws/credentials
-sed -i "s|AWS_SECRET_ACCESS_KEY|${AWS_SECRET_ACCESS_KEY}|" /home/dreip/.aws/credentials
+cp /home/${APP_USER}/.aws/credentials_template /home/${APP_USER}/.aws/credentials
+sed -i "s|AWS_ACCESS_KEY_ID|${AWS_ACCESS_KEY_ID}|" /home/${APP_USER}/.aws/credentials
+sed -i "s|AWS_SECRET_ACCESS_KEY|${AWS_SECRET_ACCESS_KEY}|" /home/${APP_USER}/.aws/credentials
