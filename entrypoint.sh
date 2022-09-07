eeb5e64b5fe5036b888ffdfd007c94055deae10f
Matthew Bardsley <Matthew.Bardsley@warwick.ac.uk>
2022-09-07 21:01:42 +0100

    set-sms-region-india: region is now an ENV variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
 cp $HOME/.aws/credentials_template $HOME/.aws/credentials
 sed -i "s|AWS_ACCESS_KEY_ID|${AWS_ACCESS_KEY_ID}|" $HOME/.aws/credentials
 sed -i "s|AWS_SECRET_ACCESS_KEY|${AWS_SECRET_ACCESS_KEY}|" $HOME/.aws/credentials
+
+cp $HOME/.aws/config_template $HOME/.aws/config
+sed -i "s|AWS_REGION|${AWS_REGION}|" $HOME/.aws/config
